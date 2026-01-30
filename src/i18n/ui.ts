8dad4d9d80e1d5bb2df1d94ec37b7cfec1da922a
YohYamasaki <y.yamasaki@tuta.com>
2026-01-30 21:29:03 +0100

    fix: head title and description

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,7 +14,10 @@ export const langParams = Object.keys(LANGUAGES).map((lang) => ({
   params: { lang: lang as Language },
 }));
 
-type TranslationKey = "about.mainText";
+type TranslationKey =
+  | "about.mainText"
+  | "blogIndex.title"
+  | "blogIndex.description";
 
 type Translation = Record<TranslationKey, string>;
 
@@ -22,9 +25,15 @@ export const translations: Record<Language, Translation> = {
   en: {
     "about.mainText":
       "I am a software engineer and a computer science student based in Germany with a keen interest in the intersection of web technology and 2D graphics.",
+    "blogIndex.title": "Blog",
+    "blogIndex.description":
+      "Here are some of my miscellaneous articles about software",
   },
   ja: {
     "about.mainText":
       "ドイツでWEBエンジニアをしています。パートタイムのコンピューターサイエンス学生でもある。2DグラフィックスとWEB技術に興味があります。",
+    "blogIndex.title": "ブログ",
+    "blogIndex.description":
+      "主にUniversity of London Computer Science BScのことなどの雑記",
   },
 } as const;
